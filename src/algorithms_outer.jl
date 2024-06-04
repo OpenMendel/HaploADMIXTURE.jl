2ccb30992f94c4a7785a07981c88f098ef804983
Seyoon Ko <kos@ucla.edu>
2024-06-03 20:29:30 -0700

    Update algorithms_outer.jl

--- src/algorithms_outer.jl
+++ src/algorithms_outer.jl
@@ -37,7 +37,8 @@ Initialize P and Q with the FRAPPE EM algorithm
 """
 function admixture_qn!(d::AdmixData2{T, T2}, g::AbstractArray{T2}, iter::Int=1000, 
     rtol= 1e-7; d_cu=nothing, g_cu=nothing, mode=:ZAL, iter_count_offset=0, penalty=nothing, 
-    fix_q=false, fix_p=false, em_p=false, em_p_iter=10) where {T, T2}
+    fix_q=false, fix_p=false, em_p=false, em_p_iter=10,
+        guarantee_increase=false) where {T, T2}
     # qf!(d.qf, d.q, d.f)
     # ll_prev = loglikelihood(g, d.q, d.f, d.qp_small, d.K, d.skipmissing)
     # d.ll_new = ll_prev
@@ -67,13 +68,13 @@ function admixture_qn!(d::AdmixData2{T, T2}, g::AbstractArray{T2}, iter::Int=100
             # ll_prev = loglikelihood(g, d.qf)
             d.ll_prev = d.ll_new
             if !fix_q
-                update_q!(d, g; d_cu=d_cu, g_cu=g_cu, penalty=q_penalty)
+                update_q!(d, g; d_cu=d_cu, g_cu=g_cu, penalty=q_penalty, guarantee_increase=guarantee_increase)
             else
                 d.q_next .= d.q
             end
             if !fix_p
                 if !em_p
-                    update_p!(d, g; d_cu=d_cu, g_cu=g_cu, penalty=p_penalty)
+                    update_p!(d, g; d_cu=d_cu, g_cu=g_cu, penalty=p_penalty, guarantee_increase=guarantee_increase)
                 else
                     for ii in 1:em_p_iter
                         em!(d, g; d_cu=d_cu, g_cu=g_cu, fix_q=true, fix_p=false)
@@ -90,13 +91,13 @@ function admixture_qn!(d::AdmixData2{T, T2}, g::AbstractArray{T2}, iter::Int=100
                 d.p_next .= d.p
             end
             if !fix_q
-                update_q!(d, g, true; d_cu=d_cu, g_cu=g_cu, penalty=q_penalty)
+                update_q!(d, g, true; d_cu=d_cu, g_cu=g_cu, penalty=q_penalty, guarantee_increase=guarantee_increase)
             else
                 d.q_next2 .= d.q
             end
             if !fix_p
                 if !em_p
-                    update_p!(d, g, true; d_cu=d_cu, g_cu=g_cu, penalty=p_penalty)
+                    update_p!(d, g, true; d_cu=d_cu, g_cu=g_cu, penalty=p_penalty, guarantee_increase=guarantee_increase)
                 else
                     for ii in 1:em_p_iter
                         em!(d, g; d_cu=d_cu, g_cu=g_cu, fix_q=true, fix_p=false)
